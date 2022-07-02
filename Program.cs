335a753605ccc810cf7576bb6670b2f0aa277a0e
JediaelMoura <108358231+JediaelMoura@users.noreply.github.com>
2022-07-02 01:11:50 -0300

    Ex_Conversa

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿class Program{
-static void Main(string[] args){
-    Console.WriteLine("Olá Mundo!");
-}
+﻿using System;
+using static System.Console;
+
+public class Nome_Idade
+{
+   static void Main()
+    {
+        Console.WriteLine("Olá, por favor, qual é o seu nome?");
+        string nome = Console.ReadLine();   // Vai aguardar a digitação do usuário.	 
+        Console.WriteLine("Olá " + nome + " seja bem vindo!\n");
+
+        Console.WriteLine(nome + ", e qual é a sua profissão?");
+        string profissao = Console.ReadLine();
+        Console.WriteLine("Que legal " + nome + ", você é " + profissao + "!\n");
+
+        Console.WriteLine(nome + ", você gosta de ser " + profissao + "?");
+        string resposta = Console.ReadLine();
+        Console.WriteLine(resposta + "... top D+, adorei conversar com você.\n");
+        Console.WriteLine("bye bye...");
+    }
 }
